1eda60338dfa6b9f2ffad809b7ae7039ceefb13b
zcy205321334-bit <zcy205321334@gmail.com>
2026-08-16 21:32:18 +0800

    chore: 版本号升至 v2.1.0, 更新检查地址指向新仓库

--- src/meta.py
+++ src/meta.py
@@ -5,10 +5,10 @@ VideoSlim 应用程序常量定义
 """
 
 # 应用程序版本号
-VERSION = "v2.0.0"
+VERSION = "v2.1.0"
 
 # 检查更新的 GitHub API URL
-CHECK_UPDATE_URL = "https://api.github.com/repos/DongGuoZheng/VideoSlim/releases"
+CHECK_UPDATE_URL = "https://api.github.com/repos/zcy205321334-bit/VideoSlim/releases"
 
 # 支持的视频文件扩展名列表
 SUPPORTED_VIDEO_EXTENSIONS = [".mp4", ".mkv", ".mov", ".avi"]
